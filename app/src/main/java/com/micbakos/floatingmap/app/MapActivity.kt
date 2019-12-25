c1c75fc2c30d5aa3434cd621e8530e293e8c25dd
micbakos <micbakos@gmail.com>
2019-12-25 18:35:36 +0200

    Add google maps into project

---
+++ app/src/main/java/com/micbakos/floatingmap/app/MapActivity.kt
@@ -0,0 +1,13 @@
+package com.micbakos.floatingmap.app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MapActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+    }
+
+}
