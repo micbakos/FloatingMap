7e54cdcd4daed5ac74997576a65b03686609540e
micbakos <micbakos@gmail.com>
2019-12-26 20:10:13 +0200

    Create and attach google map callbacks

--- floatingmap/src/main/kotlin/com/micbakos/floatingmap/FloatingMap.kt
+++ floatingmap/src/main/kotlin/com/micbakos/floatingmap/FloatingMap.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.SupportMapFragment
 
 class FloatingMap : SupportMapFragment() {
 
     private lateinit var googleMapView: View
     private lateinit var mapOverlay: MapOverlay
+    private lateinit var googleMap: GoogleMap
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,6 +23,31 @@ class FloatingMap : SupportMapFragment() {
 
         mapOverlay = MapOverlay(requireContext()).apply {
             addView(googleMapView)
+        }
+
+        getMapAsync { map ->
+            this.googleMap = map
+
+            with(map) {
+                val googleMapCallbacks = GoogleMapCallbacks()
+
+                setOnCameraIdleListener(googleMapCallbacks)
+                setOnCameraMoveCanceledListener(googleMapCallbacks)
+                setOnCameraMoveStartedListener(googleMapCallbacks)
+                setOnCameraMoveListener(googleMapCallbacks)
+                setOnMapLoadedCallback(googleMapCallbacks)
+            }
+
+            with(map.uiSettings) {
+                isZoomGesturesEnabled = true
+                isRotateGesturesEnabled = false
+                isTiltGesturesEnabled = false
+                isCompassEnabled = false
+                isIndoorLevelPickerEnabled = false
+                isMapToolbarEnabled = false
+                isMyLocationButtonEnabled = false
+                isZoomControlsEnabled = false
+            }
         }
 
         return mapOverlay
---
+++ floatingmap/src/main/kotlin/com/micbakos/floatingmap/GoogleMapCallbacks.kt
@@ -0,0 +1,31 @@
+package com.micbakos.floatingmap
+
+import com.google.android.gms.maps.GoogleMap
+
+internal class GoogleMapCallbacks : GoogleMap.OnMapLoadedCallback,
+    GoogleMap.OnCameraMoveStartedListener,
+    GoogleMap.OnCameraMoveListener,
+    GoogleMap.OnCameraMoveCanceledListener,
+    GoogleMap.OnCameraIdleListener {
+
+    override fun onMapLoaded() {
+
+    }
+
+    override fun onCameraMoveStarted(p0: Int) {
+
+    }
+
+    override fun onCameraMove() {
+
+    }
+
+    override fun onCameraMoveCanceled() {
+
+    }
+
+    override fun onCameraIdle() {
+
+    }
+
+}
